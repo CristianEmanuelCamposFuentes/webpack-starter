3b75ee2244b619f66a64075a5e38293d764e2caa
Emanuel <cristianemanuelcamposfuentes@hotmail.com>
2022-03-12 23:03:30 -0300

    Inicializacion del proyecto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { saludar } from './js/componentes';
+import './styles.css';
+
+
+const nombre = ' Cristian Emanuel ';
+
+saludar( nombre );
---
+++ src/js/componentes.js
@@ -0,0 +1,22 @@
+import '../css/componentes.css';
+// import webpacklogo from '../assets/img/webpack-logo.png';
+
+
+export const saludar = ( nombre = 'sin nombre') => {
+
+    console.log( 'Creando etiqueta h1 ');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola ${ nombre }!`;
+
+    document.body.append( h1 )
+    
+    
+    // IMG
+    // console.log( webpacklogo );
+    // const img = document.createElement('img');
+    // img.src = webpacklogo;
+    // document.body.append(img);
+    // 
+};
+
